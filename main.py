94686824b70a3d378b33b05e136485211f8271a8
hamzaraouzi <hamza.raouzi2016@gmail.com>
2025-01-10 01:01:19 +0100

    verion 2 of the counting app, with multithreading

--- main.py
+++ main.py
@@ -1,13 +1,101 @@
 import click
 from occupancy import Occupancy
+from queue import Queue
+import threading
+from msghandler import MessageHandler
+import cv2
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+osd_queue = Queue()
+msg_queue = Queue()
+
+
+def inference(source: str, model: str):
+    occupancy = Occupancy(source, model,
+                          osd_queue=osd_queue,
+                          msg_queue=msg_queue)
+    occupancy.run()
+
+
+def event_streaming(bootstrap_server: str, topic: str):
+    handler = MessageHandler(bootstrap_server=bootstrap_server,
+                             topic=topic)
+    while True:
+        data = msg_queue.get()
+        handler.send_event(**data)
+
+
+def osd_rtsp_stream(ip="localhost", port=8080):
+    """
+    Retrieve frames from the queue and stream them using GStreamer.
+    
+    Parameters:
+    ip (str): The IP address of the RTSP server. Default is 'localhost'.
+    port (int): The port number for the RTSP server. Default is 8080.
+    """
+    gst_out = (
+        f"appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=ultrafast "
+        f"! rtph264pay config-interval=1 pt=96 ! udpsink host={ip} port={port}"
+    )
+
+    # GStreamer output setup
+    fps = 25  # Adjust based on input stream's FPS
+    width, height = 1280, 720  #Adjust based on  input stream resolution
+
+    out = cv2.VideoWriter(
+        gst_out,
+        cv2.CAP_GSTREAMER,
+        0,  # Codec ID (0 for raw pipeline)
+        fps,
+        (width, height),
+    )
+
+    stop_signal = False
+    if not out.isOpened():
+        logging.error(f"Error: Could not open GStreamer pipeline for output to {ip}:{port}.")
+        stop_signal = True
+        return
+
+    while not stop_signal:
+        # Get a frame from the queue
+        frame = osd_queue.get()
+        out.write(frame)
+
+    out.release()
 
 
 @click.command()
 @click.option("--source", type=str, help="rtsp link or video path")
 @click.option("--model", type=str, help="model artifact")
-def main(source, model):
-    occupancy = Occupancy(source, model)
-    occupancy.run()
+@click.option("--bootstrap_server", type=str, default="localhost:9092", help="ip:port for kafka broker")
+@click.option("--topic", type=str, help="kafka topic where to push event")
+@click.option("--ip_rtsp", type=str, default="localhost",
+              help="ip for output rtsp stream")
+@click.option("--port_rtsp", type=str, default="8080", help="port for output rtsp stream")
+def main(source, model, bootstrap_server, topic, ip_rtsp, port_rtsp):
+    inference_thread = threading.Thread(target=inference,
+                                        kwargs={"model": model,
+                                                "source": source})
+    # event_streaming_thread = threading.Thread(target=event_streaming,
+    #                                          kwargs={"bootstrap_server":
+    #                                                  bootstrap_server,
+    #                                                  "topic": topic})
+
+    osd_rtsp_stream_thread = threading.Thread(target=osd_rtsp_stream,
+                                              kwargs={"port": port_rtsp,
+                                                      "ip": ip_rtsp})
+
+    inference_thread.start()
+    # event_streaming_thread.start()
+    osd_rtsp_stream_thread.start()
+
+    inference_thread.join()
+    # event_streaming_thread.join()
+    osd_rtsp_stream_thread.join()
 
 
 if __name__ == "__main__":
--- occupancy.py
+++ occupancy.py
@@ -3,32 +3,31 @@ from ultralytics import YOLO
 import logging
 from utils import (calculate_center, has_crossed_line, prepare_osd_frames,
                    update_obj_history, write_output_video)
-from msghandler import MessageHandler
-
+from  queue import Queue
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class Occupancy:
-    def __init__(self, source, model):
+    def __init__(self, source, model, osd_queue: Queue, msg_queue: Queue):
         self.model = YOLO(model).to("cuda")
         self.cap = cv2.VideoCapture(source)
         self.line = [(750, 200), (950, 1250)]
-        self.msg_handler = MessageHandler(bootstrap_server="", topic="")
+        self.msg_queue = msg_queue
+        self.osd_queue = osd_queue
 
         if not self.cap.isOpened():
             logger.error("Error: Unable to open RTSP stream.")
             exit()
 
         self.obj_history = dict()
-        self.osd_buffer = list()
 
     def process_tracks(self, results, frame):
         for result in results:
             for box in result.boxes:
                 object_id = int(box.id.cpu().numpy()[0])
                 bbox = box.xyxy[0].cpu().numpy().tolist()
-                cls = int(box.cls.cpu()) #TODO read the class
+                cls = int(box.cls.cpu())
                 center = calculate_center(bbox=bbox)
                 prev_center = self.obj_history.get(object_id, None)
                 line_is_crossed, direction = has_crossed_line(prev_center,
@@ -36,7 +35,8 @@ class Occupancy:
                                                               self.line)
                 if line_is_crossed:
                     logging.info(f"Object {object_id}: {direction} ")
-                    self.msg_handler.send_event(direction, cls)
+                    self.msg_queue.put({"classe": cls,
+                                        "direction": direction})
 
                 self.obj_history = update_obj_history(
                     object_histories=self.obj_history,
@@ -48,7 +48,7 @@ class Occupancy:
                                                line=self.line,
                                                obj_id=object_id)
 
-                self.osd_buffer.append(osd_frame)
+            self.osd_queue.put(osd_frame)
 
     def run(self):
         frame_skip_cnt = 0
@@ -64,4 +64,4 @@ class Occupancy:
             results = self.model.track(source=frame, persist=True, tracker="bytetrack.yaml")
             self.process_tracks(results=results, frame=frame)
         
-        write_output_video(self.osd_buffer, "out.mp4", fps=25)
+        #write_output_video(self.osd_buffer, "out.mp4", fps=25)

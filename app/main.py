02bfd6f2627c61c00a46f59864205ae693566107
hamzaraouzi <hamza.raouzi2016@gmail.com>
2025-01-16 20:51:54 +0100

    add ip_rtsp & port_rtsp

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def event_streaming(bootstrap_server: str, topic: str):
         handler.send_event(**data)
 
 
-def out_rtsp_stream():
+def out_rtsp_stream(ip_rtsp, port_rtsp):
     while True:
         frame = osd_queue.get()
         print("render out frame")
@@ -40,24 +40,34 @@ def out_rtsp_stream():
 @click.option("--model", type=str, help="model artifact")
 @click.option("--bootstrap_server", type=str, default="localhost:9092", help="ip:port for kafka broker")
 @click.option("--topic", type=str, help="kafka topic where to push event")
-def main(source, model, bootstrap_server, topic):
+@click.option("--ip_rtsp", type=str, default="localhost",
+              help="ip for output rtsp stream")
+@click.option("--port_rtsp", type=str, default="8080", help="port for output rtsp stream")
+def main(source, model, bootstrap_server, topic, ip_rtsp, port_rtsp):
     source = os.getenv("source", source)
     bootstrap_server = os.getenv("bootstrap_server", bootstrap_server) 
     topic = os.getenv("topic", topic)
+    ip_rtsp = os.getenv("ip_rtsp", ip_rtsp)
+    port_rtsp = os.getenv("ip_rtsp", port_rtsp)
 
     inference_thread = threading.Thread(target=inference,
                                         kwargs={"model": model,
                                                 "source": source})
-    # event_streaming_thread = threading.Thread(target=event_streaming,
+    #event_streaming_thread = threading.Thread(target=event_streaming,
     #                                          kwargs={"bootstrap_server":
     #                                                  bootstrap_server,
     #                                                  "topic": topic})
+    osd_rtsp_stream_thread = threading.Thread(target=out_rtsp_stream,
+                                              kwargs={"port": port_rtsp,
+                                                      "ip": ip_rtsp})
 
     inference_thread.start()
     # event_streaming_thread.start()
+    osd_rtsp_stream_thread.start()
 
     inference_thread.join()
     # event_streaming_thread.join()
+    osd_rtsp_stream_thread.join()
 
 
 if __name__ == "__main__":

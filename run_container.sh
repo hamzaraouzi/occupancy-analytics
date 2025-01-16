e0b63be1ec2322cd32fe4138ddcd4d771a46feed
hamzaraouzi <hamza.raouzi2016@gmail.com>
2025-01-16 21:01:29 +0100

    add run_container script

---
+++ run_container.sh
@@ -0,0 +1,6 @@
+docker run --gpus all \
+    -e source=rtsp://35.184.184.200:8080/melen_2.mp4\
+    -e bootstrap_server=localhost:9094 \
+    -e topic=my-topic \
+    -e ip_rtsp=8080 \
+    docker.io/hamzaraouzi/people-counting
